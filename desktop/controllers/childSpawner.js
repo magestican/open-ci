085c330bf3d504cfc1059679b1583f0c2dfc5201
magestican <magestican>
2016-01-28 23:07:30 +1100

    any powershell of bat file is now executable

--- desktop/controllers/childSpawner.js
+++ desktop/controllers/childSpawner.js
@@ -1,38 +1,58 @@
 var spawn = require("child_process").spawn,
   fs = require("fs"),
   fileSystemHelper = require("../helpers/fileSystemHelper.js"),
-child;
+  scriptsSupported,
+  child;
 var path = require("path");
-var filesToExecute;
+var filesToExecute = [];
 
 var configDump = path.resolve(path.join(__dirname, '../..', '/configurations/'));
+
+
+scriptsSupported = [{
+  extension: "ps1",
+  executable: "powershell.exe"
+}, {
+  extension: "bat",
+  executable: "cmd.exe"
+}]
 
 fileSystemHelper.walk(configDump, function(err, results) {
   if (err)
     console.log(err)
   else
-    filesToExecute = results;
+    for (var i in results) {
+      for (var x in scriptsSupported)
+        var supported = scriptsSupported[x];
+      var file = results[i];
+      if (file.indexOf(.extension) > -1) {
+        filesToExecute.push({
+          RUN: supported.executable,
+          THISFILE: file
+        });
+      }
+    }
 })
-
-scriptsSupported = [{
-  "ps1" : "powershell.exe"
-},{
-  "bat" : "cmd.exe"
-}]
 
 module.exports = {
   run: function() {
 
-    child = spawn("powershell.exe", filesToExecute);
-    child.stdout.on("data", function(data) {
-      console.log("Powershell Data: " + data);
-    });
-    child.stderr.on("data", function(data) {
-      console.log("Powershell Errors: " + data);
-    });
-    child.on("exit", function() {
-      console.log("Powershell Script finished");
-    });
-    child.stdin.end(); //end input
+    for (var runthis in filesToExecute) {
+      if (object.hasOwnProperty(script)) {
+
+        child = spawn(script.RUN, script.THISFILE);
+        child.stdout.on("data", function(data) {
+          console.log("Data: " + data);
+        });
+        child.stderr.on("data", function(data) {
+          console.log("Errors: " + data);
+        });
+        child.on("exit", function() {
+          console.log("Script finished");
+        });
+        child.stdin.end(); //end input
+      }
+    }
+
   }
 }
